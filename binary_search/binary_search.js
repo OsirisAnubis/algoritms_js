3c329b77ce6a9e8927c2cdfc03abc1941d0a53a1
OsirisAnubis <olegshulgakalendar@gmail.com>
2022-02-05 11:06:50 +0300

    add binary search

---
+++ binary_search/binary_search.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const binarySearch = (array, element) => {
+  let i = 0;
+  let j = array.length - 1;
+  let mid = Math.floor((i + j) / 2);
+  while (i !== j) {
+    if (array[mid] > element) {
+      j = mid - 1;
+      mid = Math.floor((i + j) / 2);
+    }
+    else if (array[mid] < element) {
+      i = mid + 1;
+      mid = Math.floor((i + j) / 2);
+    }
+    else {
+      return true;
+    }
+  }
+  if (array[mid] === element) return true;
+  else return false;
+};
+
+//Example
+const sortedArray = [1, 5, 8, 10, 15, 20];
+console.log(binarySearch(sortedArray, 1));//true
+console.log(binarySearch(sortedArray, 5));//true
+console.log(binarySearch(sortedArray, 23));//false
+console.log(binarySearch(sortedArray, 9));//false
